772c9f014571239c1595f1131fa5a5816b9853c4
Yumesh-Kumar <yumeshkumarsewak@gmail.com>
2022-05-27 22:10:58 +0530

    setting done fot practice

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ScreenHeight } from 'react-native-elements/dist/helpers';
-import CGJSActivityIndicator from './components/ActivityIndicator/ActivityIndicator';
-import Carousel from './components/Carousel/Carousel';
-import Header from './components/Header/Header';
-import Buttons from './components/Shared Components/Buttons';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-      <Header />
-      <View style={{top:70}} >
-  <Carousel/>
-      </View>
+     <Text>Hello</Text>
     
     </View>
     </>
